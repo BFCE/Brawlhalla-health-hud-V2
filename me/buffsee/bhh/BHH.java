05f09c8e7fe1101f1a3303e59e6b797ff3fdf35c
DESKTOP-5499RID\bfce <werg@gmail.com>
2018-12-23 02:43:55 -0800

    Massively reduced memory and CPU footprint -- down to about 80-100MB memory usage & 0% CPU when running from the debugger in Eclipse. I suspect the native compilation will be even better.

---
+++ me/buffsee/bhh/BHH.java
@@ -0,0 +1,120 @@
+package me.buffsee.bhh;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class BHH extends Thread {
+	public JFrame bhh = new JFrame("BHH");
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		Robot robot = null;
+
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		JLabel healthleft = new JLabel("? | ?", SwingConstants.CENTER);
+		healthleft.setFont(new Font("Verdana", Font.PLAIN, 42));
+		
+		bhh.add(healthleft);
+		bhh.setUndecorated(true);
+		bhh.setOpacity(0.8f);
+		bhh.setSize(300, 100);
+		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+		bhh.setLocation((int) (screensize.getWidth()) - 300, (int) screensize.getHeight() / 10);
+		bhh.setAlwaysOnTop(true);
+
+		//Rectangle screenRect = new Rectangle(0, 0, Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
+		
+		Rectangle screenRectLeft = new Rectangle((int) (screensize.getWidth() / 1.1), ((int) screensize.getHeight() / 12)-1, (int) (screensize.getWidth() / 1.1)+1, (int) screensize.getHeight() / 12);
+		Rectangle screenRectRight = new Rectangle((int) (screensize.getWidth() / 1.04), ((int) screensize.getHeight() / 12)-1, (int) (screensize.getWidth() / 1.04)+1, (int) screensize.getHeight() / 12);
+
+		System.out.println(screensize.getHeight() / 12);
+		robot.mouseMove((int) (screensize.getWidth() / 1.1), 120);
+		while (true) {
+			if (bhh.isVisible()) {
+				String toplefthealth = String.valueOf((int)this.getHealthFromPixel(0, 0, robot, screenRectLeft));
+				String toprighthealth = String.valueOf((int)this.getHealthFromPixel(0, 0, robot, screenRectRight));
+			
+				healthleft.setText(toplefthealth + " | " + toprighthealth);
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public double getHealthFromColor(int[] tst) {
+		double algorithm = (765 - (tst[0] + tst[1] + tst[2]))/5;
+		if(algorithm >= 51) 
+			algorithm = (609 - (tst[0] + tst[1] + tst[2]))/2;
+		
+		if(algorithm > 100) 
+			algorithm = (707 - (tst[0] + tst[1] + tst[2]))/3;
+		
+		if(algorithm > 150)
+			algorithm = 345.802-(((tst[0] + tst[1] + tst[2]))/0.655)/2;
+		
+		if(algorithm > 201) {
+			algorithm = 390 - (tst[0] + tst[1] + tst[2]);
+		}
+		if(algorithm > 251) {
+			algorithm = 357.1 - (tst[0] + tst[1] + tst[2]) * 0.7692307692307692;
+		}
+		return algorithm;
+	}
+	
+	public double getHealthFromPixel(int x, int y, Robot robot, Rectangle rect) {
+		BufferedImage grid = robot.createScreenCapture(rect);
+		int tst[] = null;
+		Raster k = grid.getData();
+		tst = k.getPixel(x, y, tst);
+		return this.getHealthFromColor(tst);
+	}
+	
+	public void hideWindow() {
+		bhh.setVisible(false);
+	}
+
+	public void showWindow() {
+		bhh.setVisible(true);
+	}
+
+	public void startExplorer() {
+		try {
+			Runtime.getRuntime().exec("explorer.exe");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void stopExplorer() {
+		try {
+			Runtime.getRuntime().exec("taskkill /f /im explorer.exe");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ me/buffsee/bhh/Main.java
@@ -0,0 +1,69 @@
+package me.buffsee.bhh;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame BHHMainFrame = new JFrame("BHH v2");
+		
+		
+		BHH bhhthread = new BHH();
+		bhhthread.start();
+		
+		
+		JCheckBox BHHCheckBox = new JCheckBox();
+		BHHCheckBox.setLocation(50, 0);
+		BHHCheckBox.setSize(18, 18);
+		BHHCheckBox.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(bhhthread.bhh.isVisible()) {
+					bhhthread.hideWindow();
+					bhhthread.startExplorer();
+				} else {
+					bhhthread.showWindow();
+					bhhthread.stopExplorer();
+				}
+			}
+		});
+	
+		
+		JTextArea BHHText = new JTextArea("BHH 1v1:");
+		BHHText.setLocation(0, 0);
+		BHHText.setSize(25, 16);
+		
+		JTextArea BHHText2s = new JTextArea("2v2:");
+		BHHText2s.setLocation(75, 0);
+		BHHText2s.setSize(25, 16);
+		
+
+		BHHMainFrame.add(BHHCheckBox);
+		BHHMainFrame.add(BHHText2s);
+		BHHMainFrame.add(BHHText);
+
+		BHHMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		BHHMainFrame.addWindowListener(new WindowAdapter()
+		{
+		    public void windowClosing(WindowEvent e)
+		    {
+		    	if(bhhthread.bhh.isVisible()) {
+		    		bhhthread.startExplorer();
+		    	}
+		    }
+		});
+		BHHMainFrame.setVisible(true);
+		BHHMainFrame.setSize(300, 500);
+	}
+
+}
